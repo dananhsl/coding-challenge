789a7f75f79935a6979cdf95e85a846503111a7b
Skadi <skadi@DESKTOP-R7AD8VS.localdomain>
2023-01-30 23:42:40 +0100

    add all questions + script to format questions from plain text to array of objects

--- src/questions.js
+++ src/questions.js
@@ -3,56 +3,644 @@
     {
       "id": 1,
       "question": "Es liegt mir eher,",
-      "answers":[ "wenn man mir etwas durch die Blume sagt.","wenn man „das Kind beim Namen nennt."],
-      "scores": ["", ""]
+      "answers":[ 
+        "wenn man mir etwas durch die Blume sagt.",
+        "wenn man „das Kind beim Namen nennt."
+    ],
+      "scores": [
+      "", 
+      ""
+    ]
     },
     {
       "id": 2,
       "question": "",
-      "answers":[ "Ich denke manchmal lange nach und sage dann trotzdem nichts.","Ich rede manchmal schneller als ich denke."],
-      "scores": ["", ""]
+      "answers":[ 
+        "Ich denke manchmal lange nach und sage dann trotzdem nichts.",
+        "Ich rede manchmal schneller als ich denke."
+    ],
+      "scores": [
+      "", 
+      ""
+    ]
     },
     {
         "id": 3,
         "question": "Langfristige Pläne geben mir das Gefühl,",
-        "answers":[ "in einer Zwangsjacke zu stecken., ","in einer Zwangsjacke zu stecken."],
-        "scores": ["", ""]
+        "answers":[ 
+            "in einer Zwangsjacke zu stecken.",
+            "in einer Zwangsjacke zu stecken."
+    ],
+        "scores": [
+        "", 
+        ""
+    ]
       },
       {
         "id": 4,
         "question": "Arbeiten, die Geduld und Sorgfalt erfordern, ",
-        "answers":[ "sind für mich eher eine Quälerei.","machen mir meistens Spaß."],
-        "scores": ["", ""]
+        "answers":[ 
+            "sind für mich eher eine Quälerei.",
+            "machen mir meistens Spaß."
+    ],
+        "scores": [
+        "", 
+        ""
+    ]
       },
       {
         "id": 5,
         "question": "Ich neige eher dazu,",
-        "answers":[ "ehrlich meine Meinung zu sagen.","auf Harmonie und Ausgleich bedacht zu sein."],
-        "scores": ["", ""]
+        "answers":[ 
+            "ehrlich meine Meinung zu sagen.",
+            "auf Harmonie und Ausgleich bedacht zu sein."
+    ],
+        "scores": [
+            "", 
+            ""
+        ]
       },
       {
         "id": 6,
         "question": "Für mich gilt eher:",
-        "answers":[ "Arbeit und Freizeit sind bei mir nicht so klar voneinander getrennt.","Erst die Arbeit, dann das Vergnügen."],
-        "scores": ["", ""]
+        "answers":[ 
+            "Arbeit und Freizeit sind bei mir nicht so klar voneinander getrennt.",
+            "Erst die Arbeit, dann das Vergnügen."
+        ],
+        "scores": [
+            "", 
+            ""
+        ]
       },
       {
         "id": 7,
         "question": "Wenn ich eine Entscheidung zu treffen habe, höre ich im Zweifelsfall auf",
-        "answers":[ "meinen Bauch.","meinen Verstand."],
-        "scores": ["", ""]
+        "answers":[ 
+            "meinen Bauch.",
+            "meinen Verstand."
+        ],
+        "scores": [
+            "", 
+            ""
+        ]
       },
       {
         "id": 8,
         "question": "Wenn ich bastle oder handwerke, ",
-        "answers":[ "tue ich dies sorgfältig und genau.","tue ich dies eher „Pi mal Daumen“."],
-        "scores": ["", ""]
+        "answers":[ 
+            "tue ich dies sorgfältig und genau.",
+            "tue ich dies eher „Pi mal Daumen“."
+        ],
+        "scores": [
+            "",
+            ""
+        ]
       },
       {
         "id": 9,
         "question": "Wenn ich mich entscheiden müsste, würde ich mich eher bezeichnen als",
-        "answers":[ "Realisten.","Idealisten."],
-        "scores": ["", ""]
+        "answers":[ 
+            "Realisten.",
+            "Idealisten."
+        ],
+        "scores": [
+            "", 
+            ""
+        ]
       },
+      {
+        "id": 10,
+        "question": "",
+        "answers": [
+          "Bei mir hat alles seinen Platz.",
+          "Bei mir herrscht eher „kreatives Chaos\"."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 11,
+        "question": "Meiner Meinung nach ist die größere Tugend",
+        "answers": [
+          "Direktheit.",
+          "Taktgefühl."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 12,
+        "question": "Meine Stärke liegt eher darin,",
+        "answers": [
+          "gut zu organisieren",
+          "flexibel auf Unerwartetes zu reagieren"
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 13,
+        "question": "Wenn ich in Gesellschaft bin,",
+        "answers": [
+          "höre ich viel zu.",
+          "rede ich gern und viel."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 14,
+        "question": "Wenn ich Probleme habe,",
+        "answers": [
+          "spreche ich, wenn überhaupt, mit wenigen Menschen darüber.",
+          "trage ich mein Herz auf der Zunge."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 15,
+        "question": "",
+        "answers": [
+          "Ich würde mich eher als Gefühlsmenschen bezeichnen.",
+          "Ich würde mich eher als Kopfmenschen bezeichnen."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 16,
+        "question": "Wenn ich Gäste erwarte,",
+        "answers": [
+          "habe ich meine Vorbereitungen in der Regel schon eine viertel Stunde, bevor die Gäste kommen, abgeschlossen.",
+          "bin ich noch bei den Vorbereitungen, während die Ersten eintreffen."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 17,
+        "question": "",
+        "answers": [
+          "Ich habe eine gute Wahrnehmung für Details.",
+          "Ich habe eine schlechte Wahrnehmung für Details."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 18,
+        "question": "",
+        "answers": [
+          "Ich weiß am Montag bereits gerne, was ich am Wochenende vorhabe.",
+          "Ich lasse das Wochenende am liebsten auf mich zukommen."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 19,
+        "question": "Ich tanke meine Batterien am besten auf,",
+        "answers": [
+          "wenn ich in netter Gesellschaft bin.",
+          "wenn ich alleine bin."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 20,
+        "question": "Es macht mir mehr Spaß,",
+        "answers": [
+          "etwas Neues zu entwerfen.",
+          "einen bestehenden Entwurf so lange zu überarbeiten, bis er funktioniert."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 21,
+        "question": "Es nervt mich eher,",
+        "answers": [
+          "wenn Leute sich nicht an Verabredungen halten.",
+          "wenn Leute unflexibel sind."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 22,
+        "question": "",
+        "answers": [
+          "Meine Gefühle sind mir meist recht deutlich anzumerken.",
+          "Andere können nicht so leicht erkennen, was in mir vorgeht."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 23,
+        "question": "",
+        "answers": [
+          "Es passiert mir häufig, dass ich Sachen auf den letzten Drücker erledige.",
+          "Meistens habe ich eine gute Zeiteinteilung."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 24,
+        "question": "",
+        "answers": [
+          "Ich habe eher eine philosophische Ader.",
+          "Ich habe eher eine praktische Ader."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 25,
+        "question": "Ich entscheide und beurteile",
+        "answers": [
+          "eher objektiv/sachlich.",
+          "eher subjektiv/gefühlsmäßig."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 26,
+        "question": "Wenn ich über längere Zeit mit anderen Menschen zusammen bin,",
+        "answers": [
+          "finde ich das nicht anstrengend.",
+          "finde ich das oft ermüdend und anstrengend."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 27,
+        "question": "Ich bevorzuge eher eine Arbeit,",
+        "answers": [
+          "bei der soziale Fähigkeiten und Einfühlungsvermögen gefragt sind.",
+          "bei der rationales Handeln und analytisches Denken gefragt sind."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 28,
+        "question": "Ich verlasse mich lieber",
+        "answers": [
+          "auf meine fünf Sinne.",
+          "auf meine Intuition."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 29,
+        "question": "Wenn ich mit einer Gruppe von Freunden verreise,",
+        "answers": [
+          "kann ich diese fast ständig um mich haben.",
+          "brauche ich zwischendurch Zeit für mich allein."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 30,
+        "question": "",
+        "answers": [
+          "Ich formuliere meine Gedanken und Gefühle meist spontan.",
+          "Ich überlege meist länger, bevor ich meine Gedanken und Gefühle äußere."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 31,
+        "question": "Ich mag es,",
+        "answers": [
+          "wenn jemand eine bildreiche Sprache benutzt und seine Rede ausschmückt.",
+          "wenn sich jemand präzise ausdrückt und schnell zum Punkt kommt."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 32,
+        "question": "Ich arbeite lieber in einem Bereich,",
+        "answers": [
+          "in dem viele neue Konzepte gefragt sind.",
+          "in dem es auf Genauigkeit und Faktenwissen ankommt."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 33,
+        "question": "",
+        "answers": [
+          "Ich ahne schon früh, wenn sich zwei verlieben.",
+          "Ich bin oft einer der letzten, der merkt, wenn es zwischen zweien gefunkt hat."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 34,
+        "question": "",
+        "answers": [
+          "Unangenehme Dinge packe ich meistens an, um sie hinter mich zu bringen.",
+          "Unangenehme Dinge schiebe ich häufig auf die lange Bank."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 35,
+        "question": "Wenn ich auf einer Party bin,",
+        "answers": [
+          "spreche ich eher nicht so gern fremde Menschen an.",
+          "spreche ich gern fremde Menschen an."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 36,
+        "question": "Wenn ich vor einer schwierigen Aufgabe stehe,",
+        "answers": [
+          "lege ich los, wenn ich mir eine grobe Skizze über meine Vorgehensweise gemacht habe.",
+          "plane ich die einzelnen Schritte recht genau."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 37,
+        "question": "",
+        "answers": [
+          "Ich erlebe meine Gefühle eher spontan und intensiv.",
+          "Ich bin eher nicht so schnell in Gefühlswallungen zu versetzen."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 38,
+        "question": "Ich arbeite am effektivsten",
+        "answers": [
+          "am Ende, wenn die Zeit drängt.",
+          "im Vorfeld, wenn ich weiß, dass mir noch genügend Zeit bleibt."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 39,
+        "question": "Es bereitet mir mehr Spaß,",
+        "answers": [
+          "mich über Sachthemen zu unterhalten.",
+          "mich über zwischenmenschliche Beziehungen zu unterhalten."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      },
+      {
+        "id": 40,
+        "question": "",
+        "answers": [
+          "Ich sinniere gern über die „großen Fragen des Lebens“.",
+          "Ich wende mich lieber handfesten Dingen zu."
+        ],
+        "scores": [
+          "",
+          ""
+        ]
+      }
       
     ]
+
+
+
+ /*   const text = `1. Es liegt mir eher, *
+wenn man mir etwas durch die Blume sagt.
+wenn man „das Kind beim Namen nennt“.
+2. *
+Ich denke manchmal lange nach und sage dann trotzdem nichts.
+Ich rede manchmal schneller als ich denke.
+3. Langfristige Pläne geben mir das Gefühl, *
+in einer Zwangsjacke zu stecken.
+alles im Griff zu haben.
+4. Arbeiten, die Geduld und Sorgfalt erfordern, *
+sind für mich eher eine Quälerei.
+machen mir meistens Spaß.
+5. Ich neige eher dazu, *
+ehrlich meine Meinung zu sagen.
+auf Harmonie und Ausgleich bedacht zu sein.
+6. Für mich gilt eher: *
+Arbeit und Freizeit sind bei mir nicht so klar voneinander getrennt.
+Erst die Arbeit, dann das Vergnügen.
+7. Wenn ich eine Entscheidung zu treffen habe, höre ich im Zweifelsfall auf *
+meinen Bauch.
+meinen Verstand.
+8. Wenn ich bastle oder handwerke, *
+tue ich dies sorgfältig und genau.
+tue ich dies eher „Pi mal Daumen“.
+9. Wenn ich mich entscheiden müsste, würde ich mich eher bezeichnen als *
+Realisten.
+Idealisten.
+10. *
+Bei mir hat alles seinen Platz.
+Bei mir herrscht eher „kreatives Chaos".
+11. Meiner Meinung nach ist die größere Tugend *
+Direktheit.
+Taktgefühl.
+12. Meine Stärke liegt eher darin, *
+gut zu organisieren
+flexibel auf Unerwartetes zu reagieren
+13. Wenn ich in Gesellschaft bin, *
+höre ich viel zu.
+rede ich gern und viel.
+14. Wenn ich Probleme habe, *
+spreche ich, wenn überhaupt, mit wenigen Menschen darüber.
+trage ich mein Herz auf der Zunge.
+15. *
+Ich würde mich eher als Gefühlsmenschen bezeichnen.
+Ich würde mich eher als Kopfmenschen bezeichnen.
+16. Wenn ich Gäste erwarte, *
+habe ich meine Vorbereitungen in der Regel schon eine viertel Stunde, bevor die Gäste kommen, abgeschlossen.
+bin ich noch bei den Vorbereitungen, während die Ersten eintreffen.
+17. *
+Ich habe eine gute Wahrnehmung für Details.
+Ich habe eine schlechte Wahrnehmung für Details.
+18. *
+Ich weiß am Montag bereits gerne, was ich am Wochenende vorhabe.
+Ich lasse das Wochenende am liebsten auf mich zukommen.
+19. Ich tanke meine Batterien am besten auf, *
+wenn ich in netter Gesellschaft bin.
+wenn ich alleine bin.
+20. Es macht mir mehr Spaß, *
+etwas Neues zu entwerfen.
+einen bestehenden Entwurf so lange zu überarbeiten, bis er funktioniert.
+21. Es nervt mich eher, *
+wenn Leute sich nicht an Verabredungen halten.
+wenn Leute unflexibel sind.
+22. *
+Meine Gefühle sind mir meist recht deutlich anzumerken.
+Andere können nicht so leicht erkennen, was in mir vorgeht.
+23. *
+Es passiert mir häufig, dass ich Sachen auf den letzten Drücker erledige.
+Meistens habe ich eine gute Zeiteinteilung.
+24. *
+Ich habe eher eine philosophische Ader.
+Ich habe eher eine praktische Ader.
+25. Ich entscheide und beurteile *
+eher objektiv/sachlich.
+eher subjektiv/gefühlsmäßig.
+26. Wenn ich über längere Zeit mit anderen Menschen zusammen bin, *
+finde ich das nicht anstrengend.
+finde ich das oft ermüdend und anstrengend.
+27. Ich bevorzuge eher eine Arbeit, *
+bei der soziale Fähigkeiten und Einfühlungsvermögen gefragt sind.
+bei der rationales Handeln und analytisches Denken gefragt sind.
+28. Ich verlasse mich lieber *
+auf meine fünf Sinne.
+auf meine Intuition.
+29. Wenn ich mit einer Gruppe von Freunden verreise, *
+kann ich diese fast ständig um mich haben.
+brauche ich zwischendurch Zeit für mich allein.
+30. *
+Ich formuliere meine Gedanken und Gefühle meist spontan.
+Ich überlege meist länger, bevor ich meine Gedanken und Gefühle äußere.
+31. Ich mag es, *
+wenn jemand eine bildreiche Sprache benutzt und seine Rede ausschmückt.
+wenn sich jemand präzise ausdrückt und schnell zum Punkt kommt.
+32. Ich arbeite lieber in einem Bereich, *
+in dem viele neue Konzepte gefragt sind.
+in dem es auf Genauigkeit und Faktenwissen ankommt.
+33. *
+Ich ahne schon früh, wenn sich zwei verlieben.
+Ich bin oft einer der letzten, der merkt, wenn es zwischen zweien gefunkt hat.
+34. *
+Unangenehme Dinge packe ich meistens an, um sie hinter mich zu bringen.
+Unangenehme Dinge schiebe ich häufig auf die lange Bank.
+35. Wenn ich auf einer Party bin, *
+spreche ich eher nicht so gern fremde Menschen an.
+spreche ich gern fremde Menschen an.
+36. Wenn ich vor einer schwierigen Aufgabe stehe, *
+lege ich los, wenn ich mir eine grobe Skizze über meine Vorgehensweise gemacht habe.
+plane ich die einzelnen Schritte recht genau.
+37. *
+Ich erlebe meine Gefühle eher spontan und intensiv.
+Ich bin eher nicht so schnell in Gefühlswallungen zu versetzen.
+38. Ich arbeite am effektivsten *
+am Ende, wenn die Zeit drängt.
+im Vorfeld, wenn ich weiß, dass mir noch genügend Zeit bleibt.
+39. Es bereitet mir mehr Spaß, *
+mich über Sachthemen zu unterhalten.
+mich über zwischenmenschliche Beziehungen zu unterhalten.
+40. *
+Ich sinniere gern über die „großen Fragen des Lebens“.
+Ich wende mich lieber handfesten Dingen zu.
+`;
+
+var res = [];
+var lines = text.split('\n');
+var cur = {};
+
+for (var i = 0; i < lines.length; i++) {
+    if (i % 3 === 0) {
+        if (Object.keys(cur).length !== 0) {
+            cur.scores = ["", ""];
+            res.push(cur);
+        }
+
+        let line = lines[i];
+        let id = line.split('.')[0];
+        let q = line.split('.')[1];
+
+        if (q === undefined) {
+            q = "";
+        } else {
+            q = q.replace('*', '');
+        }
+
+        cur = {
+            id: parseInt(id),
+            question: q.trim()
+        };
+    } else if (i % 3 === 1) {
+        let line = lines[i];
+        cur.answers = [];
+        cur.answers.push(line);
+    } else if (i % 3 === 2) {
+        let line = lines[i];
+        cur.answers.push(line);
+    }
+}
+
+console.log(res); 
+*/
