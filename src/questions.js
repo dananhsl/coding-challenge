5f041f904d3ba2dfa036d6f45a3f2c2ab5411582
Skadi <skadi@DESKTOP-R7AD8VS.localdomain>
2023-01-30 18:36:58 +0100

    add a few test questions

---
+++ src/questions.js
@@ -0,0 +1,58 @@
+
+[
+    {
+      "id": 1,
+      "question": "Es liegt mir eher,",
+      "answers":[ "wenn man mir etwas durch die Blume sagt.","wenn man „das Kind beim Namen nennt."],
+      "scores": ["", ""]
+    },
+    {
+      "id": 2,
+      "question": "",
+      "answers":[ "Ich denke manchmal lange nach und sage dann trotzdem nichts.","Ich rede manchmal schneller als ich denke."],
+      "scores": ["", ""]
+    },
+    {
+        "id": 3,
+        "question": "Langfristige Pläne geben mir das Gefühl,",
+        "answers":[ "in einer Zwangsjacke zu stecken., ","in einer Zwangsjacke zu stecken."],
+        "scores": ["", ""]
+      },
+      {
+        "id": 4,
+        "question": "Arbeiten, die Geduld und Sorgfalt erfordern, ",
+        "answers":[ "sind für mich eher eine Quälerei.","machen mir meistens Spaß."],
+        "scores": ["", ""]
+      },
+      {
+        "id": 5,
+        "question": "Ich neige eher dazu,",
+        "answers":[ "ehrlich meine Meinung zu sagen.","auf Harmonie und Ausgleich bedacht zu sein."],
+        "scores": ["", ""]
+      },
+      {
+        "id": 6,
+        "question": "Für mich gilt eher:",
+        "answers":[ "Arbeit und Freizeit sind bei mir nicht so klar voneinander getrennt.","Erst die Arbeit, dann das Vergnügen."],
+        "scores": ["", ""]
+      },
+      {
+        "id": 7,
+        "question": "Wenn ich eine Entscheidung zu treffen habe, höre ich im Zweifelsfall auf",
+        "answers":[ "meinen Bauch.","meinen Verstand."],
+        "scores": ["", ""]
+      },
+      {
+        "id": 8,
+        "question": "Wenn ich bastle oder handwerke, ",
+        "answers":[ "tue ich dies sorgfältig und genau.","tue ich dies eher „Pi mal Daumen“."],
+        "scores": ["", ""]
+      },
+      {
+        "id": 9,
+        "question": "Wenn ich mich entscheiden müsste, würde ich mich eher bezeichnen als",
+        "answers":[ "Realisten.","Idealisten."],
+        "scores": ["", ""]
+      },
+      
+    ]
