85e45b93bb3a5c9c55f0fbf470bf483571cb901c
Skadi <skadi@DESKTOP-R7AD8VS.localdomain>
2023-01-31 23:44:23 +0100

    read from localstorage on refresh

--- src/components/question.js
+++ src/components/question.js
@@ -1,14 +1,26 @@
 import useStore from "../hooks/useStore";
 
 export default function Question (props) {
+    useStore(state => state.transactions);
     const setAnswers = useStore(state => state.setAnswers);
+    const getAnswers = useStore(state => state.getAnswers)
+
     return (
         <section>
             <h1>{`${props.id}. ${props.question}`}</h1>            
             {props.answers.map((answer) => {
                 return (
                     <p>
-                        <input type="radio" name={props.id} value={answer} onClick={() => setAnswers(answer, props.id)}/>
+                        <input 
+                            type="radio" 
+                            name={props.id} 
+                            value={answer} 
+                            defaultChecked={
+                                getAnswers().filter((item) => item.id === props.id)[0]?.answer === answer
+                            }
+                            onChange={() => setAnswers(answer, props.id)} 
+                           
+                        />
                         {answer}
                     </p>
                 );
